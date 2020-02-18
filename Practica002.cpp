b2e3e6cd6c37395d537877c03e9ab1b39954c3ff
albertobdipn <60155012+albertobdipn@users.noreply.github.com>
2020-02-17 21:31:30 -0600

    Add files via upload

---
+++ Practica002.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    float a,b,c;
+    int opt;
+    printf("1=Suma\n 2=Resta\n 3=Multiplicacion\n 4=Division\n");
+    printf("Seleccione una opcion:");
+    scanf("%d",&opt);
+    printf("Ingrese el primer operando:");
+    scanf("%f",&a);
+    printf("Introduce el segundo operando:");
+    scanf("%f",&b);
+    switch(opt){
+case 1:
+    c=a+b;
+    break;
+case 2:
+    c=a-b;
+    break;
+case 3:
+    c=a*b;
+    break;
+case 4:
+    while(b==0){
+        printf("Error, b debe ser distinto de 0\n");
+        printf("Introduce el segundo operando:");
+        scanf("%f",&b);
+    }
+    c=a/b;
+    default:;
+    }
+    printf("el resultado es %f",c);
+}
+
+
